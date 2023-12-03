5005706851848893a30e5d94550c57f497e520ec
Vlad Hociota <vghociot@edu.uwaterloo.ca>
2023-12-03 18:46:09 +0000

    feat: added tailwind and flowbite

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_03_074052) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_03_090913) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.decimal "latitude"
+    t.decimal "longitude"
+    t.string "address"
+    t.integer "designation"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
